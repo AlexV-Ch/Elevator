d7ec05ed5ba0563168d73583f2b72906ec1399c1
AlexV-Ch <alexch@yandex.ru>
2024-11-29 20:47:25 +0300

    Готово

--- src/Elevator.java
+++ src/Elevator.java
@@ -15,15 +15,15 @@ public class Elevator {
     }
     //Метод moveDown -  перемещающий лифт на один этаж вниз (уменьшающий значение переменной currentFloor на единицу)
     public void moveDown() {
-        if (currentFloor > minFloor) {
+        //if (currentFloor > minFloor) {
             currentFloor = currentFloor - 1;
-        }
+        //}
     }
     // Метод moveUp, перемещающий лифт на один этаж вверх
     public void moveUp() {
-        if (currentFloor < maxFloor) {
+        //if (currentFloor < maxFloor) {
             currentFloor = currentFloor + 1;
-        }
+        //}
     }
     //Метод move(int floor), перемещающий лифт на заданный в параметре этаж, если он задан верно.
     // Если параметр у метода задан неверно, ничего не делать и выводить в консоль сообщение об ошибке.
@@ -31,6 +31,24 @@ public class Elevator {
     // с помощью циклов и методов moveUp и moveDown, и он должен выводить в консоль текущий этаж
     // после каждого перемещения между этажами.
     public void move(int floor) {
+        if (floor >= minFloor && floor <= maxFloor) {
+            System.out.println("Вы на " + currentFloor +" этаже");
+            while (currentFloor != floor) {
+                if (currentFloor > floor) {
+                    moveDown();
+                    System.out.println("Вы на " + currentFloor +" этаже");
+                } else  {
+                    moveUp();
+                    System.out.println("Вы на " + currentFloor +" этаже");
+                }
+
+            }
+            System.out.println("Вы приехали на " + currentFloor + " этаж");
+
+
+        } else {
+            System.out.println("Вы ошиблись при вводе. Нет такого этажа");
+        }
 
     }
 }
