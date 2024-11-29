42f752622b85603a0584d784953bd9dfd4673484
auto_1 <alexch@yandex.ru>
2024-11-29 15:23:01 +0300

    Ком 2

--- src/Elevator.java
+++ src/Elevator.java
@@ -1,2 +1,28 @@
-package PACKAGE_NAME;public class Elevator {
+public class Elevator {
+    private int currentFloor = 1; //(текущий этаж)
+    private int minFloor;     //минимальный этаж
+    private int maxFloor;     //максимальный этаж
+
+    // конструктор
+    public Elevator(int minFloor, int maxFloor) {
+        this.minFloor = minFloor;
+        this.maxFloor = maxFloor;
+    }
+
+    // Метод getCurrentFloor, возвращающий текущий этаж, на котором находится лифт
+    public int getCurrentFloor() {
+        return currentFloor;
+    }
+    //Метод moveDown -  перемещающий лифт на один этаж вниз (уменьшающий значение переменной currentFloor на единицу)
+    public void moveDown() {
+        if (currentFloor > minFloor) {
+            currentFloor = currentFloor - 1;
+        }
+    }
+    // Метод moveUp, перемещающий лифт на один этаж вверх
+    public void moveUp() {
+        if (currentFloor < maxFloor) {
+            currentFloor = currentFloor + 1;
+        }
+    }
 }
